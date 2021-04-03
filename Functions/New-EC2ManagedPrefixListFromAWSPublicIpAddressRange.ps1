dcbbd546259decab440e6148f5aa421289405277
nekrassov01 <unclek62@gmail.com>
2021-04-04 01:07:44 +0900

    Add parameter "ProfileName"

--- Functions/New-EC2ManagedPrefixListFromAWSPublicIpAddressRange.ps1
+++ Functions/New-EC2ManagedPrefixListFromAWSPublicIpAddressRange.ps1
@@ -28,82 +28,103 @@ Function New-EC2ManagedPrefixListFromAWSPublicIpAddressRange
         [ValidateNotNullOrEmpty()]
         [string]$Region,
 
-        [Parameter(Position = 1, Mandatory = $true)]
+        [Parameter(Position = 1, Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [string]$ProfileName = 'default',
+
+        [Parameter(Position = 2, Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
         [string[]]$ServiceKey,
 
-        [Parameter(Position = 2, Mandatory = $false)]
-        [ValidateSet('Ipv4','Ipv6')]
+        [Parameter(Position = 3, Mandatory = $false)]
+        [ValidateSet('Ipv4', 'Ipv6')]
         [string]$IpAddressFormat,
 
-        [Parameter(Position = 3, Mandatory = $true)]
+        [Parameter(Position = 4, Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
         [int]$MaxEntry,
 
-        [Parameter(Position = 4, Mandatory = $true)]
+        [Parameter(Position = 5, Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
         [string]$PrefixListName
     )
 
     begin
     {
-        Set-DefaultAWSRegion -Region $region
-        Import-Module -Name AWS.Tools.EC2
-
-        $ipv4Entries = @()
-        $ipv6Entries = @()
-    }
-
-    process
-    {
-        $AWSPublicIpAddresses = Get-AWSPublicIpAddressRange -ServiceKey $serviceKey -Region $region
-
-        foreach ($awsPublicIpAddress In $awsPublicIpAddresses)
+        try
         {
-            if ($awsPublicIpAddress.IpPrefix -like '*.*' -and $ipAddressFormat -eq 'Ipv4')
+            Set-StrictMode -Version Latest
+            Set-DefaultAWSRegion -Region $Region
+            Import-Module -Name AWS.Tools.EC2
+
+            $isExist = Get-EC2ManagedPrefixList -Filter @{ Name="prefix-list-name"; Values=$prefixListName } -ProfileName $profileName
+
+            if ($isExist.State.Value -eq 'create-complete')
             {
-                $ipv4Entry = New-Object -TypeName Amazon.EC2.Model.AddPrefixListEntry
-                $ipv4Entry.Cidr = $awsPublicIpAddress.IpPrefix
-                $ipv4Entry.Description = $awsPublicIpAddress.Service
-                $ipv4Entries += $ipv4Entry
+                $isExist
+                return
             }
 
-            if ($awsPublicIpAddress.IpPrefix -like '*:*' -and $ipAddressFormat -eq 'Ipv6')
+            $AWSPublicIpAddresses = Get-AWSPublicIpAddressRange -ServiceKey $serviceKey -Region $region
+            
+            $ipv4Entries = @()
+            $ipv6Entries = @()
+            
+            foreach ($awsPublicIpAddress In $awsPublicIpAddresses)
             {
-                $ipv6Entry = New-Object -TypeName Amazon.EC2.Model.AddPrefixListEntry
-                $ipv6Entry.Cidr = $awsPublicIpAddress.IpPrefix
-                $ipv6Entry.Description = $awsPublicIpAddress.Service
-                $ipv6Entries += $ipv6Entry
+                if ($awsPublicIpAddress.IpPrefix -like '*.*' -and $ipAddressFormat -eq 'Ipv4')
+                {
+                    $ipv4Entry = New-Object -TypeName Amazon.EC2.Model.AddPrefixListEntry
+                    $ipv4Entry.Cidr = $awsPublicIpAddress.IpPrefix
+                    $ipv4Entry.Description = $awsPublicIpAddress.Service
+                    $ipv4Entries += $ipv4Entry
+                }
+            
+                if ($awsPublicIpAddress.IpPrefix -like '*:*' -and $ipAddressFormat -eq 'Ipv6')
+                {
+                    $ipv6Entry = New-Object -TypeName Amazon.EC2.Model.AddPrefixListEntry
+                    $ipv6Entry.Cidr = $awsPublicIpAddress.IpPrefix
+                    $ipv6Entry.Description = $awsPublicIpAddress.Service
+                    $ipv6Entries += $ipv6Entry
+                }
             }
+            
+            $tags = @{ Key='Name'; Value=$prefixListName }
+            $nameTag = New-Object -TypeName Amazon.EC2.Model.TagSpecification
+            $nameTag.ResourceType = 'prefix-list'
+            $nameTag.Tags.Add($tags)
+            
+            $param = @{
+                ProfileName = $profileName
+                AddressFamily = $ipAddressFormat
+                MaxEntry = $maxEntry
+                PrefixListName = $prefixListName
+                TagSpecification = $nameTag
+            }
+            
+            if ($ipAddressFormat -eq 'Ipv4')
+            {
+                $param.Add('Entry', $ipv4Entries)
+            }
+            
+            if ($ipAddressFormat -eq 'Ipv6')
+            {
+                $param.Add('Entry', $ipv6Entries)
+            }
+
+            $prefixList = New-EC2ManagedPrefixList @param
         }
-
-        $tags = @{ Key='Name'; Value=$prefixListName }
-        $nameTag = New-Object -TypeName Amazon.EC2.Model.TagSpecification
-        $nameTag.ResourceType = 'prefix-list'
-        $nameTag.Tags.Add($tags)
-
-        $param = @{
-            AddressFamily = $ipAddressFormat
-            MaxEntry = $maxEntry
-            PrefixListName = $prefixListName
-            TagSpecification = $nameTag
-        }
-
-        if ($ipAddressFormat -eq 'Ipv4')
+        catch
         {
-            $param.Add('Entry', $ipv4Entries)
+            $PSCmdlet.ThrowTerminatingError($PSItem)
         }
-
-        if ($ipAddressFormat -eq 'Ipv6')
-        {
-            $param.Add('Entry', $ipv6Entries)
-        }
-
-        $prefixList = New-EC2ManagedPrefixList @param
     }
-    
+
     end
     {
-        return $prefixList
+        if (Test-Path -LiteralPath 'Variable:prefixList')
+        {
+            return $prefixList
+        }
     }
 }
--- Functions/New-EC2SecurityGroupFromAWSPublicIpAddressRange.ps1
+++ Functions/New-EC2SecurityGroupFromAWSPublicIpAddressRange.ps1
@@ -22,7 +22,7 @@ New-EC2SecurityGroupFromAWSPublicIpAddressRange @param
 Author: nekrassov01
 #>
 
-Function New-EC2SecurityGroupFromAWSPublicIpAddressRange
+function New-EC2SecurityGroupFromAWSPublicIpAddressRange
 {
     [CmdletBinding()]
     [OutputType([void])]
@@ -32,106 +32,119 @@ Function New-EC2SecurityGroupFromAWSPublicIpAddressRange
         [ValidateNotNullOrEmpty()]
         [string]$Region,
 
-        [Parameter(Position = 1, Mandatory = $true)]
+        [Parameter(Position = 1, Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [string]$ProfileName = 'default',
+
+        [Parameter(Position = 2, Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
         [string[]]$ServiceKey,
 
-        [Parameter(Position = 2, Mandatory = $false)]
-        [ValidateSet('Ipv4','Ipv6')]
+        [Parameter(Position = 3, Mandatory = $true)]
+        [ValidateSet('Ipv4', 'Ipv6')]
         [string]$IpAddressFormat,
 
-        [Parameter(Position = 3, Mandatory = $true)]
+        [Parameter(Position = 4, Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
         [string]$GroupName,
         
-        [Parameter(Position = 4, Mandatory = $true)]
+        [Parameter(Position = 5, Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
         [string]$Description,
         
-        [Parameter(Position = 5, Mandatory = $true)]
+        [Parameter(Position = 6, Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
         [string]$VpcId,
         
-        [Parameter(Position = 6, Mandatory = $true)]
+        [Parameter(Position = 7, Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
         [string]$IpProtocol,
 
-        [Parameter(Position = 7, Mandatory = $true)]
+        [Parameter(Position = 8, Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
         [int]$FromPort,
         
-        [Parameter(Position = 8, Mandatory = $true)]
+        [Parameter(Position = 9, Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
         [int]$ToPort
     )
 
     begin
     {
-        Set-DefaultAWSRegion -Region $Region
-        Import-Module -Name AWS.Tools.EC2
-
-        $Ipv4Ranges = @()
-        $Ipv6Ranges = @()
-    }
-
-    process
-    {
-        $tags = @{ Key='Name'; Value=$groupName }
-        $nameTag = New-Object -TypeName Amazon.EC2.Model.TagSpecification
-        $nameTag.ResourceType = 'security-group'
-        $nameTag.Tags.Add($tags)
-
-        $param = @{
-            GroupName = $groupName
-            Description = $description
-            VpcId = $vpcId
-            TagSpecification = $nameTag
-        }
-        $groupId = New-EC2SecurityGroup @param
-
-        $awsPublicIpAddresses = Get-AWSPublicIpAddressRange -ServiceKey $serviceKey -Region $region
-
-        foreach ($awsPublicIpAddress In $awsPublicIpAddresses)
+        try
         {
-            if ($awsPublicIpAddress.IpPrefix -like '*.*')
+            Set-StrictMode -Version Latest
+            Set-DefaultAWSRegion -Region $Region
+            Import-Module -Name AWS.Tools.EC2
+
+            $tags = @{ Key='Name'; Value=$groupName }
+            $nameTag = New-Object -TypeName Amazon.EC2.Model.TagSpecification
+            $nameTag.ResourceType = 'security-group'
+            $nameTag.Tags.Add($tags)
+
+            $param = @{
+                ProfileName = $profileName
+                GroupName = $groupName
+                Description = $description
+                VpcId = $vpcId
+                TagSpecification = $nameTag
+            }
+            $groupId = New-EC2SecurityGroup @param
+
+            $awsPublicIpAddresses = Get-AWSPublicIpAddressRange -ServiceKey $serviceKey -Region $region
+
+            $Ipv4Ranges = @()
+            $Ipv6Ranges = @()
+
+            foreach ($awsPublicIpAddress In $awsPublicIpAddresses)
             {
-                $ipv4Range = New-Object -TypeName Amazon.EC2.Model.IpRange
-                $ipv4Range.CidrIp = $awsPublicIpAddress.IpPrefix
-                $ipv4Range.Description = $awsPublicIpAddress.Service
-                $ipv4Ranges += $ipv4Range
+                if ($awsPublicIpAddress.IpPrefix -like '*.*')
+                {
+                    $ipv4Range = New-Object -TypeName Amazon.EC2.Model.IpRange
+                    $ipv4Range.CidrIp = $awsPublicIpAddress.IpPrefix
+                    $ipv4Range.Description = $awsPublicIpAddress.Service
+                    $ipv4Ranges += $ipv4Range
+                }
+
+                if ($awsPublicIpAddress.IpPrefix -like '*:*')
+                {
+                    $ipv6Range = New-Object -TypeName Amazon.EC2.Model.Ipv6Range
+                    $ipv6Range.CidrIpv6 = $awsPublicIpAddress.IpPrefix
+                    $ipv6Range.Description = $awsPublicIpAddress.Service
+                    $ipv6Ranges += $ipv6Range
+                }
             }
 
-            if ($awsPublicIpAddress.IpPrefix -like '*:*')
+            $ipPermission = New-Object -TypeName Amazon.EC2.Model.IpPermission
+            $ipPermission.IpProtocol = $ipProtocol
+            $ipPermission.FromPort = $fromPort
+            $ipPermission.ToPort = $toPort
+            $ipPermission.Ipv4Ranges = $ipv4Ranges
+            $ipPermission.Ipv6Ranges = $ipv6Ranges
+
+            if ($ipAddressFormat -eq 'Ipv4')
             {
-                $ipv6Range = New-Object -TypeName Amazon.EC2.Model.Ipv6Range
-                $ipv6Range.CidrIpv6 = $awsPublicIpAddress.IpPrefix
-                $ipv6Range.Description = $awsPublicIpAddress.Service
-                $ipv6Ranges += $ipv6Range
+                $ipPermission.Ipv6Ranges.Clear()
             }
-        }
-
-        $ipPermission = New-Object -TypeName Amazon.EC2.Model.IpPermission
-        $ipPermission.IpProtocol = $ipProtocol
-        $ipPermission.FromPort = $fromPort
-        $ipPermission.ToPort = $toPort
-        $ipPermission.Ipv4Ranges = $ipv4Ranges
-        $ipPermission.Ipv6Ranges = $ipv6Ranges
-
-        if ( $ipAddressFormat -eq 'Ipv4' )
-        {
-            $ipPermission.Ipv6Ranges.Clear()
-        }
         
-        if ( $ipAddressFormat -eq 'Ipv6' )
-        {
-            $ipPermission.Ipv4Ranges.Clear()
-        }
+            if ($ipAddressFormat -eq 'Ipv6')
+            {
+                $ipPermission.Ipv4Ranges.Clear()
+            }
 
-        Grant-EC2SecurityGroupIngress -GroupId $groupId -IpPermission $ipPermission
+            Grant-EC2SecurityGroupIngress -GroupId $groupId -IpPermission $ipPermission
+        }
+        catch
+        {
+            $PSCmdlet.ThrowTerminatingError($PSItem)
+        }
     }
 
     end
     {
-        return Get-EC2SecurityGroup -GroupId $groupId
+        if ($groupId)
+        {
+            return Get-EC2SecurityGroup -GroupId $groupId
+        }
     }
 }
